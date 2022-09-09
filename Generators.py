07c812d7cb7a29e17b068cb52a1fc1126fd184fd
Michael Lin <michael.lin2032@gmail.com>
2022-09-09 16:19:51 -0500

    learn about generators

--- Generators.py
+++ Generators.py
@@ -1,16 +1,61 @@
-def get_odds_generator():
-    n=1
-    
-    n+=2
-    yield n
-    
-    n+=2
-    yield n 
-    
-    n+=2
+# A simple generator function
+def my_gen():
+    n = 1
+    print('This is printed first')
+    # Generator function contains yield statements
     yield n
 
-numbers=get_odds_generator()
-print(next(numbers))
-print(next(numbers))
-print(next(numbers))
+    n += 1
+    print('This is printed second')
+    yield n
+
+    n += 1
+    print('This is printed at last')
+    yield n
+
+
+simple = my_gen()
+print(next(simple))
+print(next(simple))
+print(next(simple))
+
+
+def rev_str(my_str):
+    length = len(my_str)
+    for i in range(length - 1, -1, -1):
+        yield my_str[i]
+
+
+# For loop to reverse the string
+for char in rev_str("hello"):
+    print(char)
+
+
+my_list = [1, 3, 6, 10]
+generator = (x**2 for x in my_list)
+print(generator)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+
+
+# Generator expressions can be used as function arguments. 
+# When used in such a way, the round parentheses can be dropped.
+
+
+print(sum(x**2 for x in my_list))
+print(max(x**2 for x in my_list))
+
+
+def fibonacci_numbers(nums):
+    x, y = 0, 1
+    for _ in range(nums):
+        x, y = y, x+y
+        yield x
+
+def square(nums):
+    for num in nums:
+        yield num**2
+
+print(sum(square(fibonacci_numbers(10))))
