94057ea072b0c404a5993ed4d401807134ec0404
Michael Lin <michael.lin2032@gmail.com>
2022-10-22 15:01:14 -0500

    did more general stuff

--- ScratchPad.py
+++ ScratchPad.py
@@ -2,3 +2,6 @@ import math
 # print(math.log2(math.factorial(21)*math.factorial(5)))
 
 print(math.log2(26))
+print(math.log2(math.factorial(21)*math.factorial(5))/2.7)
+
+print(((20+12+20+14+9+8+18+9+11+9)*1600)/10)
