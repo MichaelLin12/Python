e9dd88574f77e74545325c10598e6114bfe5bf1c
Michael Lin <michael.lin2032@gmail.com>
2022-10-31 21:53:55 -0500

    tried to np

--- ScratchPad.py
+++ ScratchPad.py
@@ -1,7 +1,12 @@
 import math
+import numpy as np
 # print(math.log2(math.factorial(21)*math.factorial(5)))
 
 print(math.log2(26))
 print(math.log2(math.factorial(21)*math.factorial(5))/2.7)
 
 print(((20+12+20+14+9+8+18+9+11+9)*1600)/10)
+
+X = np.asarray([3,-2,-1,0])
+Y = np.asarray([4,-1,0,2])
+print(np.linalg.inv(np.transpose(X)*X)*np.transpose(X)*y)
