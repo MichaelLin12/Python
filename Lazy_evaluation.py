506c4baa27ee68cbe1f430be246961258a21db20
Michael Lin <michael.lin2032@gmail.com>
2022-11-21 01:10:07 -0600

    learning

--- Lazy_evaluation.py
+++ Lazy_evaluation.py
@@ -4,6 +4,7 @@ def lazy_property(fn):
 
     @property
     def _lazy_property(self):
+
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
