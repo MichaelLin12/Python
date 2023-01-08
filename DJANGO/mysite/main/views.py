236cf87d89a4035759efa68dc0ed91639640121d
MichaelLin12 <michael.lin2032@gmail.com>
2023-01-08 16:50:20 -0600

    basic of django

--- DJANGO/mysite/main/views.py
+++ DJANGO/mysite/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList,Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>tech with tim!</h1>")
-
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
+def index(response,id):
+    ls = ToDoList.objects.get(id=id)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s <br/> %s</h1> " %(ls.name,str(items.text)))
